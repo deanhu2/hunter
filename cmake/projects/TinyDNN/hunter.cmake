28b394b3afe31e4705c59584d0defaacd25a2a0e
Dean <de.hunte@gmail.com>
2017-12-13 17:14:22 +0000

    updated tiny dnn

--- cmake/projects/TinyDNN/hunter.cmake
+++ cmake/projects/TinyDNN/hunter.cmake
@@ -12,11 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     TinyDNN
     VERSION
-    1.0.6
+    1.0.7
     URL
-    "https://github.com/deanhu2/hunter-tiny-dnn/archive/1.06.tar.gz"
+    "https://github.com/deanhu2/hunter-tiny-dnn/archive/1.07.tar.gz"
     SHA1
-    8b1d211896aacb4574d520170354534c75bf436e
+    b8e66c59d6b3cdf24c533cf27b593f383bca4295
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
