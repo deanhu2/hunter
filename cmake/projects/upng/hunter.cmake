e317e0a3f0f5e4330d190acb1a31f85ae8b32de3
Dean <de.hunte@gmail.com>
2017-12-14 13:46:29 +0000

    updated upng

--- cmake/projects/upng/hunter.cmake
+++ cmake/projects/upng/hunter.cmake
@@ -12,11 +12,11 @@ hunter_add_version(
     PACKAGE_NAME
     upng
     VERSION
-    1.0.2
+    1.0.3
     URL
-    "https://github.com/deanhu2/hunter-upng/archive/1.02.tar.gz"
+    "https://github.com/deanhu2/hunter-upng/archive/1.03.tar.gz"
     SHA1
-    a9658380effed20239e634605fcc804ce9809e7b
+    87f5738356d2042d00bf806e33fcbd941a90d362
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
